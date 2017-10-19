c1319d6618080b1f8895fc35275ca662287039c4
James Keen <jk15863@bristol.ac.uk>
2017-10-19 23:50:06 +0100

    It works this time

--- Duffing.py
+++ Duffing.py
@@ -12,33 +12,34 @@ def Root(ys, x0):
 
     x0 = int(abs( x0 / end)* steps)
     ig = ys[x0]
-    time = 0
     range_ = 1000
-    e = 0.00000000000000000001
+    time = ((x0 + range_)*end) / steps
+    e = 0.001
     happy = False
-    while ~happy:
+    while happy == False:
 
         for i in range(range_):
-            sg = ys[x0 + i + 1]
+            sg = ys[x0 + i + 10]
 
-            if x0 + i + 2 == steps:
+            if x0 + i + 11 == steps:
                 break
             
-            elif abs(ig - sg) < e:
+            if abs(ig - sg) < e:
                 happy = True
                 time = ((x0 + i) * end) / steps
                 break
+        
         break
 
-    return [happy, round(time, 3)]
+    return [happy, time] #round(time, 7)]
 
 k = 0.05
 g = 0.2
 w = 1
 
 start = 0
-end = 10
-steps = 20000
+end = 200
+steps = 2000
 
 
 U0 = [0, 0]
@@ -48,7 +49,7 @@ ys = Us[:,0]
 
 
 
-print(Root(ys, 0))
+print(Root(ys, 140))
 
 
 
