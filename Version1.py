d89926f8d82f0d917d4f63456c3ffba9099cc0fd
James Keen <jk15863@bristol.ac.uk>
2017-11-17 01:45:26 +0000

    in need of neatening but submissable

--- Version1.py
+++ Version1.py
@@ -1,12 +1,12 @@
 ####### Import useful things ############
 
-import scipy.integrate 
+import scipy.integrate
 import matplotlib.pyplot as plt
 #from math import *
 from scipy.optimize import fsolve
 from numpy import *
 from numpy.matlib import repmat
-from numpy import transpose
+#from numpy import transpose
 
 
 ######## Define function to integrate in first order form ##########
@@ -35,7 +35,7 @@ def zeroproblem(x, ODE, T, pars):
 
         outputs     an ODE in a form that can be passed to a root finder
     """
-    
+
     return x - scipy.integrate.odeint(ODE, x, [0, T], args = (pars,))[-1, :]
 
 
@@ -46,7 +46,7 @@ def shooting(ODE, x0, T, pars):
                     An initial guess
                     The time period to look for
                     extra parameters for the ODE
-       
+
         outputs     a root to the ode if one exists (if the initial guess is too far out of range will return NaN)
     """
 
@@ -66,38 +66,38 @@ def cheb(N):
     Generates the chebychev polynomials
 
     input:          Power of the polynomial you wish to use to approximate the integral
-    
-    
-    output:         NxN matrix of Chebychev polynomials         
+
+
+    output:         NxN matrix of Chebychev polynomials
     """
     if N == 0:
         D = 0
         x = 0
         return (D, x)                                                           ##To catch any divisions by zero
-    x = array([])                                                               ##Initialise empty arrays to fill later 
+    x = array([])                                                               ##Initialise empty arrays to fill later
     c = array(2)
 
     for i in range(N+1):
         if i <= N:                                                              ## Get the x_i values, stopping at N
-            x = append(x, cos( (pi * i ) / N  ))    
+            x = append(x, cos( (pi * i ) / N  ))
         if i == 0:
             pass                                                                ## C starts and ends with a 2 so we skip the first index of the vector
         if i == N-1:
-            
+
             c = append(c, 2*(-1)**(i-1))                                        ## append the final 2
         if i > N-2 :                                                            ## Dont double count the last entry
             pass
-            
+
         else:
             c = append(c, (-1)**(i-1))                                          ##fill the rest with +/- 1
 
     X = repmat(x,1, N+1)
     X = X.reshape(N +1, N +1)                                                   ##duplicate the column then reshape it into a matrix
-    X = X.transpose() 
+    X = X.transpose()
     dX = subtract(X , transpose(X))                                             ## construct d
-    
+
     D = divide(tensordot( c, (1/c).transpose(), axes = 0), (dX + eye(N+1)))     ##Needs to be tensor product to output a matrix
-   
+
     D + D.reshape(N+1,N+1)
     D = D - diag(sum(D, axis = 1  ))                                            ## subtracts a diagonal matrix of the sum of each row
     return (D, x)
@@ -105,7 +105,7 @@ def cheb(N):
 
 # Tests on the Chebyshev differentiation matrix routines
 
- 
+
 def testeq(M, N, name):
     """
     testeq(M, N, name)
@@ -125,7 +125,7 @@ def testeq(M, N, name):
 
 def testp(M,  name):
     """
-    
+
     """
     if all(M[len(M)-1] - M[0] < 0.001):
         print("Failed " + name)
@@ -133,7 +133,7 @@ def testp(M,  name):
     else:
         print("Passed " + name)
 
-    
+
 
 
 def runtests_cheb(cheb):
@@ -214,20 +214,45 @@ def runtests_ode(collocation):
     # Solve the first ODE on the interval [-1, 1] with no parameters and zeros
     # as the starting guess; use 21 points across the interval
     n = 20  # 21 - 1 for the degree of polynomial needed
-    
-    dim =2 
-    
-    x = cos(pi*arange(0, dim*(n+1))/n)  # the Chebyshev collocation points
-    
-    soln1 = collocation(Duffing, n, zeros(2*(n+1)),pars)
-    
+
+
+    x = cos(pi*arange(0, (n+1))/n)  # the Chebyshev collocation points
+
+    soln1 = collocation( Duffing, n, zeros(2*(n+1)), pars )
+    #soln1 = fsolve(zerocoll, zeros(n+1), pars)
     plt.plot(x, soln1)
     plt.show()
-    exactsoln1 = 1/(1+pi**2)*sin(pi*x) - pi/(1+pi**2)*cos(pi*x)
+    #exactsoln1 = 1/(1+pi**2)*sin(pi*x) - pi/(1+pi**2)*cos(pi*x)
     if linalg.norm(soln1 - exactsoln1) < 1e-6:
         print("ODE test 1 passed")
     else:
         print("ODE test 1 failed")
+
+def augmented( y, args, coll, pars):
+        return append (coll(y[:-1], pars) , dot(transpose(args[1]), (subtract(y , args[0]))))  ## define system of eqns with the pseudo-arclength encoded
+
+
+
+def zerocoll( f, N, x , pars ):
+    D, t = cheb(N)
+
+    dim = int(round(size(x) /(N+1) ))
+
+    x = reshape(x, [N+1, dim])
+    r = dot(D, x)
+
+    Y = zeros_like(x)
+    for i in range(N+1):
+        Y[i, :] = f(x[i, :], t[i], pars)
+
+    h = r - Y
+    h[-1] = x[0] - x[-1]
+
+    h.reshape(size(x),)
+
+    return h
+
+
 
 
 def run_cheb(f, N, U0, pars, vary_par, step_size, max_steps):
@@ -242,47 +267,45 @@ def run_cheb(f, N, U0, pars, vary_par, step_size, max_steps):
                 step_size - the size of the first step for the continuation
                 max_steps - the maximum number of points you would like to use during the continuation
 
-    outputs:    
+    outputs:
 
 
     """
-
-
     D, t = cheb(N)                                                          ## Generates chebyshev matrix D and points t
     par_values = []                                                         ## Initialises two emplty lists to populate later
     max_x_values = []
-    def coll(x, pars):      
+    def coll(x, pars):
 
         dim = int(round(size(x) / (N+1)))                                   ## Caluclates the order of the highest derivative
-        
+
         x = reshape(x, [N+1, dim])                                          ## reshape x into the dimensions required
-        
+
         r = dot(D, x)                                                       ## calculate D . x
 
         Y = zeros_like(x)                                                   ## create a zero array the shape of x
-        for i in range(N+1):                        
+        for i in range(N+1):
             Y[i, :] = f(x[i, :], t[i], pars)                                ## populate every row of the above array with the vals of f at t
 
         h = r - Y                                                           ## make this a list of eqns of the form D.x - f = 0
         h[-1] = x[0] - x[-1]                                                ## add the periodicity requirement
-        
+
         return h.reshape(size(x),)                                          ## reshape into a column vector for fsolve to solve
-    
-     
+
+
 
     par_values.append(pars[vary_par])                                       ## keep track of the parameters we are using / varying
 
-    x0 = fsolve(coll, U0, pars)                                             ## solve for the first point
- 
+    x0 = fsolve(zerocoll, U0, pars)                                             ## solve for the first point
+
     pars[vary_par] += step_size                                             ## change the parameter we are interested in
     par_values.append(pars[vary_par])
-    
+
     max_x_values.append(max(x0[:]))
     x1 = fsolve(coll, x0, pars)                                             ## solve for that updated set of parameters
     max_x_values.append(max(x1[:]))
 
-    def augmented( y, args):
-        return append (coll(y[:-1], y[-1]) , dot(transpose(args[1]), (subtract(y , args[0]))))  ## define system of eqns with the pseudo-arclength encoded
+    #def augmented( y, args):
+    #    return append (ODE(y[:-1], pars) , dot(transpose(args[1]), (subtract(y , args[0]))))
 
 
     y0 = append(x0, par_values[0])                                          ## define the first solutions which allow us to ...
@@ -290,7 +313,7 @@ def run_cheb(f, N, U0, pars, vary_par, step_size, max_steps):
     for i in range(max_steps):
         secant = subtract(y1, y0)                                           ## describe a line between the first two points to approximate the next step for the vary_par
         y2hat = add(y1, secant)
-        
+
         #print(y2hat)
 
         y2 = fsolve(augmented, y2hat, args = [y2hat, secant])               ## solve for the actual vary_par
@@ -308,9 +331,9 @@ def run_cheb(f, N, U0, pars, vary_par, step_size, max_steps):
 
     return 0
 
-    
+
 def quadratic(x, p):
-     
+
     """
     Equation to test the shooting continuation code
 
@@ -335,42 +358,48 @@ def cubic(x, p):
     return x**3 - x - p
 
 
+
 def Results( ODE, U0, pars, vary_par, step_size, max_steps, discretisation, solver):
     """
     This equation runs continuation with a variety of different approaches
-    inputs:     ODE - the ode to solve 
+    inputs:     ODE - the ode to solve
                 U0 - initial guess at the solution (takes different forms depending on the method you choose)
                 pars - extra parameters you would like to pass
                 vary_par - index of the variable to vary that we are interested in
                 step_size - size of step of the continuation
                 max_steps - the maximum number of points you would like for the continuation
-                discretisation - 
+                discretisation -
                 solver -
 
-    outputs:    
+    outputs:
 
     """
-    
+
     max_x_values = []
-   
-    par_values = [] 
-    
- 
+
+    par_values = []
+
+
     t = linspace(0, 2 * pi / w, 501)
-    
-   
-    if discretisation.upper() == 'SHOOTING':
-        
+
+
+    if discretisation.upper() == 'SHOOTING':  ## Actually Else:
+
         par_values.append(pars[vary_par])
 
+        pars = [1]
+
         x0 = fsolve(ODE, U0, pars)
+
         max_x_values.append(max(x0))
-        pars[vary_par] += step_size
+        pars[vary_par] += -0.1
+
+        pars = [0.9]
+
         par_values.append(pars[vary_par])
 
         x1 = fsolve(ODE, x0, pars)
         max_x_values.append(max(x1))
-
         def augmented( y, args):
 
 
@@ -378,41 +407,52 @@ def Results( ODE, U0, pars, vary_par, step_size, max_steps, discretisation, solv
 
 
 
+
+
         y0 = append( x0, par_values[0])
-        y1 = append( x1, par_values[1]) 
+        y1 = append( x1, par_values[1])
+
+
 
         for i in range(max_steps):
             secant = subtract(y1 , y0)
             y2hat = add(y1 , secant)
-            
-         
+
+
+
             y2 = fsolve( augmented, y2hat, args=[y2hat, secant])
 
-             
-            
+
+
             pars[vary_par] = y2[-1]
 
             par_values.append(y2[-1])
             max_x_values.append(max(y2[:-1]))
             y0 = y1
             y1 = y2
-        
+
         plt.plot(par_values, max_x_values)
+        plt.xlabel('Varied  Parameter')
+        plt.ylabel('Max X Value')
+        plt.suptitle('Solving BVPs with fsolve')
         plt.show()
+
+        print(par_values)
 
     if discretisation.upper() == 'CHEBYSHEV':
         N = len(U0)-1
-        
+
         run_cheb(ODE, N, U0, pars, vary_par, step_size, max_steps)
 
 
 
     else:
         print('something went wrong ...')
-        
-            
-      
+
+
+
 ######### System constants ##########
+
 k = 0.1
 g = 0.5
 w = pi
@@ -421,22 +461,24 @@ t = linspace(0, 2*pi / w, 201)
 
 pars = [k, g, w]
 
+
 U0 = [0, 1]
 
 ########################################################################
-ODE = Duffing 
-U0 = zeros([21,2])
-pars = pars
+ODE = ode1
+U0 = zeros([21, 1])
+pars = [1]
 vary_par = 0
-step_size = -0.1 
+step_size = -0.1
 max_steps = 30
 discretisation = 'chebyshev'
 solver = 0
 
-#runtests_ode( run_cheb )
+#runtests_ode( zerocoll )
 
 
-Results( ODE, U0, pars, vary_par, step_size, max_steps, discretisation, solver) 
+Results( ODE, U0, pars, vary_par, step_size, max_steps, discretisation, solver)
+
 
 
 
